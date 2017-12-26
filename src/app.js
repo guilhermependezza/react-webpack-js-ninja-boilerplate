8cec8a700cfce7f1e837048155d60d9e96ec10ed
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-26 10:56:23 -0200

    Implement initial markup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 
 import React from 'react'
 
-export default class Title extends React.Component {
-  render () {
-    return (
-      <h1>Olar</h1>
-    )
-  }
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='search'>
+        <input type='search' placeholder='Digite o nome do usuário' />
+      </div>
+      <div className='user-info'>
+        <img src='https://avatars2.githubusercontent.com/u/487669?v=4' />
+        <h1>
+          <a href='https:github.com/fdaciuk'>Fernando Daciuk</a>
+        </h1>
+
+        <ul className='repos-info'>
+          <li>- Repositorios 122</li>
+          <li>- Seguidores 10</li>
+          <li>- Seguindo 10</li>
+        </ul>
+
+        <div className='actions'>
+          <button>Ver repositórios</button>
+          <button>Ver favoritos</button>
+        </div>
+
+        <div className='repos'>
+          <h2>Repositórios</h2>
+          <ul>
+            <li><a href='#'>Nome do repositório</a></li>
+          </ul>
+        </div>
+
+        <div className='starred'>
+          <h2>Favoritos</h2>
+          <ul>
+            <li><a href='#'>Nome do repositório</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default App
